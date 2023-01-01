3d738c7365c14de659fbe5358469d6a7de034100
Fede Calendino <fede@calendino.com>
2023-01-01 12:28:18 +0100

    Refactor code

---
+++ src/formatters.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+
+
+def default(now: datetime) -> str:
+    return now.strftime("%d %B, %Y (%H%:%M:%S)")
+
+
+def iso8601(now: datetime) -> str:
+    return now.isoformat()
+
+
+def iso8601_without_microseconds(now: datetime) -> str:
+    return now.replace(microsecond=0).isoformat()
+
+
+FORMATTERS = {
+    "FORMAT_DEFAULT": default,
+    "FORMAT_ISO8601_WITH_MICROSECONDS": iso8601,
+    "FORMAT_ISO8601_WITHOUT_MICROSECONDS": iso8601_without_microseconds,
+}
--- src/main.py
+++ src/main.py
@@ -1,22 +1,17 @@
-import os
 import sys
-from uuid import uuid4
 from datetime import datetime
+from uuid import uuid4
 
 import pytz as tz
 from pyflow import Workflow
 
 import data
+import formatters
 
 
-DATE_FORMAT = os.getenv("DATE_FORMAT", "%d %B, %Y")
-TIME_FORMAT = os.getenv("TIME_FORMAT", "%H%:%M:%S")
+def get_home(workflow):
+    home_tz = workflow.env["HOME"][3:].replace("__", "/")
 
-
-def main(workflow):
-    home = workflow.env["HOME"]
-
-    home_tz = home[3:].replace("__", "/")
     home_now = (
         datetime.utcnow()
         .replace(tzinfo=tz.utc)
@@ -25,6 +20,10 @@ def main(workflow):
         )
     )
 
+    return home_tz, home_now
+
+
+def get_timezones(workflow, home_tz):
     timezones = set(
         map(
             lambda item: item[0][3:].replace("__", "/"),
@@ -34,86 +33,87 @@ def main(workflow):
             ),
         )
     )
+
     timezones.add(home_tz)
 
-    timezones = {
+    return {
         timezone: datetime.utcnow()
         .replace(tzinfo=tz.utc)
         .astimezone(tz=tz.timezone(timezone))
         for timezone in timezones
     }
 
-    for timezone, now in sorted(
-        timezones.items(), key=lambda pair: pair[1].isoformat()
-    ):
-        if timezone == home_tz:
-            icon = "img/icons/home.png"
-        elif timezone == "UTC":
-            icon = "img/icons/utc.png"
-        else:
-            utc_offset = now.utcoffset()
-            utc_offset_hours = round(
-                utc_offset.days * 24 + utc_offset.seconds / 60 / 60
-            )
-            icon = f"img/icons/{utc_offset_hours}.png"
 
+def get_formatter(workflow):
+    timestamp_format = workflow.env.get("TIMESTAMP_FORMAT", formatters.default)
+    return formatters.FORMATTERS[timestamp_format]
+
+
+def get_icon(timezone, now, home_tz):
+    if timezone == home_tz:
+        return "img/icons/home.png"
+
+    if timezone == "UTC":
+        return "img/icons/utc.png"
+
+    utc_offset = now.utcoffset()
+    utc_offset_hours = round(utc_offset.days * 24 + utc_offset.seconds / 60 / 60)
+    return f"img/icons/{utc_offset_hours}.png"
+
+
+def get_home_offset_str(timezone, home_tz, now, home_now) -> str:
+    if timezone == home_tz:
+        return ""
+
+    now_tmp = now.replace(tzinfo=None)
+    home_now_tmp = home_now.replace(tzinfo=None)
+
+    if home_now_tmp > now_tmp:
+        home_offset = home_now_tmp - now_tmp
+        seconds = home_offset.seconds + 1
+        text = "behind"
+    else:
+        home_offset = home_now_tmp - now_tmp
+        seconds = 24 * 60 * 60 - home_offset.seconds + 1
+        text = "ahead of"
+
+    return "· [{hours:02}:{minutes:02} hs {text} home 🏠]".format(
+        hours=seconds // 3600,
+        minutes=(seconds % 3600) // 60,
+        text=text,
+    )
+
+
+def main(workflow):
+    home_tz, home_now = get_home(workflow)
+    timezones = get_timezones(workflow, home_tz)
+    formatter = get_formatter(workflow)
+
+    sorter = lambda pair: pair[1].isoformat()
+
+    for timezone, now in sorted(timezones.items(), key=sorter):
         location = timezone.replace("/", ", ").replace("_", " ")
-
-        if timezone == home_tz:
-            home_offset_str = ""
-        else:
-            now_tmp = now.replace(tzinfo=None)
-            home_now_tmp = home_now.replace(tzinfo=None)
-
-            if home_now_tmp > now_tmp:
-                home_offset = home_now_tmp - now_tmp
-                seconds = home_offset.seconds + 1
-                text = "behind"
-            else:
-                home_offset = home_now_tmp - now_tmp
-                seconds = 24 * 60 * 60 - home_offset.seconds + 1
-                text = "ahead of"
-
-            home_offset_str = "· [{hours:02}:{minutes:02} hs {text} home 🏠]".format(
-                hours=seconds // 3600,
-                minutes=(seconds % 3600) // 60,
-                text=text,
-            )
-
-        # -------------------
-        # Apply formatting
-        # -------------------
-        timestamp_format = workflow.env["TIMESTAMP_FORMAT"]
-        if timestamp_format == "FORMAT_ISO8601_WITHOUT_MICROSECONDS":
-            timestamp = now.replace(microsecond=0).isoformat()
-        elif timestamp_format == "FORMAT_ISO8601_WITH_MICROSECONDS":
-            timestamp = now.isoformat()
-        else:
-            # FORMAT_DEFAULT
-            timestamp = "{time} ({date})".format(
-                time=now.strftime(TIME_FORMAT),
-                date=now.strftime(DATE_FORMAT),
-            )
+        home_offset_str = get_home_offset_str(timezone, home_tz, now, home_now)
 
         workflow.new_item(
-            title=timestamp,
+            title=formatter(now),
             subtitle="{flag} {location} {home_offset}".format(
                 flag=data.flags.get(timezone, "🌐"),
                 location=location,
                 home_offset=home_offset_str,
             ),
-            arg=timestamp,
-            copytext=timestamp,
+            arg=formatter(now),
+            copytext=formatter(now),
             valid=True,
             uid=str(uuid4()),
         ).set_icon_file(
-            path=icon,
+            path=get_icon(timezone, now, home_tz),
         ).set_alt_mod(
             subtitle="Copy ISO format (with microseconds)",
-            arg=now.isoformat(),
+            arg=formatters.iso8601(now),
         ).set_cmd_mod(
             subtitle="Copy ISO format (without microseconds)",
-            arg=now.replace(microsecond=0).isoformat(),
+            arg=formatters.iso8601_without_microseconds(now),
         )
 
 
