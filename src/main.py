c256c9bf414c66bedc6b9fa8104d238cacce5678
Marcuzzz <marc@mustbe.nl>
2023-11-25 14:06:49 +0100

    Added new functions (Meet, add_time, get_utc) (#18)
    * fix(info.plist): add missing key-value pair for new dict entry to fix plist structure
    feat(info.plist): add new script filter to display current time in different timezones
    fix(main.py): add support for adding/subtracting time in the command line argument to display time in the future/past
    fix(main.py): add UTC offset to the displayed time in each timezone
    
    feat(meet.py): add new file meet.py to the project
    refactor(meet.py): remove unused imports and commented code
    refactor(meet.py): remove unused variable BASE_DIR
    refactor(meet.py): remove unused function workflow_default
    refactor(meet.py): remove unused function convert_to_datetime
    refactor(meet.py): remove unused function get_home_
    refactor(meet.py): remove unused function get_timezones_
    refactor(meet.py): remove unused function handleinput
    refactor(meet.py): remove unused function meet
    refactor(meet.py): remove unused import JSONEncoder
    refactor(meet.py): remove unused import json
    refactor(meet.py): remove unused import formatters
    refactor(meet.py): remove unused import data
    refactor(meet.py): remove unused import pytz
    refactor(meet.py): remove unused import timedelta
    refactor(meet.py): remove unused import datetime
    refactor(meet
    
    * chore(info.plist): remove unused keyword2 configuration to clean up the file
    feat(info.plist): add new keyword configuration with required and trim properties to improve functionality and user experience
    
    ---------
    
    Co-authored-by: Marc Otten <marc.otten@bcnexxt.com>

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 import sys
-from datetime import datetime
+from datetime import timedelta, datetime
 from uuid import uuid4
 
 import pytz as tz
@@ -7,6 +7,37 @@ from pyflow import Workflow
 
 import data
 import formatters
+
+
+def add_time(arg):
+    if arg[0] == '-':
+        is_negative = True
+        arg = arg[1:]
+    else:
+        is_negative = False
+
+    if len(arg) == 0 or (len(arg.strip()) == 1 and arg.startswith('+')):
+        return 0
+
+    if arg.endswith(':'):
+        arg = arg[:-1]
+
+    arg = arg.split(':')
+
+    if len(arg) == 3:
+        hr, minute, sec = arg
+        total_seconds = int(hr) * 3600 + int(minute) * 60 + int(sec)
+    elif len(arg) == 2:
+        hr, minute = arg
+        total_seconds = int(hr) * 3600 + int(minute) * 60
+    elif len(arg) == 1:
+        hr = arg[0]
+        total_seconds = int(hr) * 3600
+
+    if is_negative:
+        total_seconds = -total_seconds
+
+    return total_seconds
 
 
 def get_home(workflow):
@@ -60,11 +91,29 @@ def get_icon(timezone, now, home_tz):
     utc_offset_hours = round(utc_offset.days * 24 + utc_offset.seconds / 60 / 60)
     return f"img/icons/{utc_offset_hours}.png"
 
+def get_utc(timezone, now, home_tz):
+    # if timezone == home_tz:
+    #     return "img/icons/home.png"
+
+    # if timezone == "UTC":
+    #     return "img/icons/utc.png"
+
+    utc_offset = now.utcoffset()
+    utc_offset_hours = round(utc_offset.days * 24 + utc_offset.seconds / 60 / 60)
+    
+    if utc_offset_hours == 0:
+        return ""
+    
+    if utc_offset_hours > 0:
+        return f" - UTC +{utc_offset_hours}"
+    
+    return f" - UTC {utc_offset_hours}"
+
 
 def get_home_offset_str(timezone, home_tz, now, home_now) -> str:
     if timezone == home_tz:
         return ""
-
+    
     now_tmp = now.replace(tzinfo=None)
     home_now_tmp = home_now.replace(tzinfo=None)
 
@@ -117,6 +166,14 @@ def get_name_replacements(workflow):
 
 
 def main(workflow):
+
+    total_seconds = 0
+    input = ''
+    if len(sys.argv) > 1:
+        arg = sys.argv[1]
+        input = ' ' + sys.argv[1]
+        total_seconds = add_time(arg)
+
     home_tz, home_now = get_home(workflow)
     timezones = get_timezones(workflow, home_tz)
     formatter = get_formatter(workflow)
@@ -129,16 +186,17 @@ def main(workflow):
         location = timezone.split("/")[-1].replace("_", " ")
         location = name_replacements.get(location, location)
 
-        home_offset_str = get_home_offset_str(timezone, home_tz, now, home_now)
+        home_offset_str = get_home_offset_str(timezone, home_tz, now, home_now) + get_utc(timezone, now, home_tz)
+        now += timedelta(seconds=total_seconds)
 
         workflow.new_item(
-            title=formatter(now),
+            title=formatter(now) + input,
             subtitle="{flag} {location} {home_offset}".format(
                 flag=data.flags.get(timezone, "🌐"),
                 location=location,
                 home_offset=home_offset_str,
             ),
-            arg=formatter(now),
+            arg=formatter(now) + get_utc(timezone, now, home_tz),
             copytext=formatter(now),
             valid=True,
             uid=str(uuid4()),
@@ -151,7 +209,6 @@ def main(workflow):
             subtitle="Copy ISO format (without microseconds)",
             arg=formatters.iso8601_without_microseconds(now),
         )
-
 
 if __name__ == "__main__":
     wf = Workflow()
